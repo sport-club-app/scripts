b3d96711a5bf38fa752de3c1d36b3354c3f4d2a5
Pablo ruan dos santos <pablords@gmail.com>
2022-05-28 18:55:58 -0300

    *

--- config.sh
+++ config.sh
@@ -14,15 +14,15 @@ echo "adicionando variavel ambiente " $SPORT_CLUB_HOME
 if [ $SHELL = "/bin/bash" ]; then
     echo "export SPORT_CLUB_HOME="$SPORT_CLUB_HOME>>~/.bashrc
     echo "export SPORT_CLUB_HOME="$SPORT_CLUB_HOME>>~/.profile
-    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.bashrc
-    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.profile
+    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml -f $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.bashrc
+    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml -f $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.profile
     source ~/.bashrc
     source ~/.profile
 else
     echo "export SPORT_CLUB_HOME="$SPORT_CLUB_HOME>>~/.zshrc
     echo "export SPORT_CLUB_HOME="$SPORT_CLUB_HOME>>~/.profile
-    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.zshrc
-    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.profile
+    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml -f $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.zshrc
+    echo "alias dc-sc='docker-compose -f $SPORT_CLUB_HOME/manager-ms/docker-compose.yml -f $SPORT_CLUB_HOME/partners-ms/docker-compose.yml -f $SPORT_CLUB_HOME/notifications-ms/docker-compose.yml'">>~/.profile
     source ~/.zshrc
     source ~/.profile
 fi
