02b03a6a2acb55e15bb54ceab728b04c2daea2b3
Pablo ruan dos santos <pablords@gmail.com>
2022-06-26 17:58:28 -0300

    feat: added payments-ms

--- clone_repo.sh
+++ clone_repo.sh
@@ -4,3 +4,4 @@ git clone git@github.com:sport-club-app/partners-ms.git
 git clone git@github.com:sport-club-app/manager-ms.git
 git clone git@github.com:sport-club-app/notifications-ms.git
 git clone git@github.com:sport-club-app/composes.git
+git clone git@github.com:sport-club-app/payments-ms.git
